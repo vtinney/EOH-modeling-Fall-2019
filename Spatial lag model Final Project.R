3933522a6fd1c6a4291e420bc9ac9363530abc27
Veronica Southerland <vtinney@gwu.edu>
2019-11-24 10:33:47 -0500

    Update Spatial lag model Final Project.R

--- Spatial lag model Final Project.R
+++ Spatial lag model Final Project.R
@@ -7,6 +7,8 @@ library(geosphere)
 library(spdep)
 library(ape)
 library(spatialreg)
+library(spatialEco)
+library(spatstat)
 
 # Set up the inputs ====================================================================
 
@@ -23,6 +25,7 @@ conc <- raster('dc.conc.tif')
 weight <- raster('weight.tif')
 smoke <- raster('smoke.tif')
 hist <- raster('hist.tif')
+dis <- raster('dis.tif')
 
 #=======================================================================
 # Model inputs
@@ -31,24 +34,28 @@ conc[conc == 0] <- NA
 hist[hist == 0] <- NA
 smoke[smoke == 0] <- NA
 weight[weight == 0] <- NA
+dis[dis == 0] <- NA
 
 lm.p1 <- rasterToPoints(conc)
 lm.p2 <- rasterToPoints(weight)
 lm.p3 <- rasterToPoints(hist)
 lm.p4 <- rasterToPoints(smoke)
+lm.p5 <- rasterToPoints(dis)
 
 df1 <- data.frame(lm.p1)
 df2 <- data.frame(lm.p2)
 df3 <- data.frame(lm.p3)
 df4 <- data.frame(lm.p4)
+df5 <- data.frame(lm.p5)
 
 colnames(df1) <- c('lon','lat','conc')
 colnames(df2) <- c('lon','lat','weight')
 colnames(df3) <- c('lon','lat','hist')
 colnames(df4) <- c('lon','lat','smoke')
+colnames(df5) <- c('lon','lat','dis')
 
-data1 <- cbind(df2, df3, df4)
-data2 <- data1[,c(1,2,3,6,9)]
+data1 <- cbind(df2, df3, df4, df5)
+data2 <- data1[,c(1,2,3,6,9,12)]
 
 mat <- distm(data2[,c('lon','lat')], df1[,c('lon','lat')], fun=distVincentyEllipsoid)
 data2$conc <- df1$conc[apply(mat, 1, which.min)]
@@ -58,6 +65,23 @@ df1 <- data2
 lm1 <- lm(conc[] ~ hist[], data=df1)
 lm2 <- lm(conc[] ~ weight[], data=df1)
 lm3 <- lm(conc[] ~ smoke[], data=df1)
+lm4 <- lm(conc[] ~ dis[], data=df1)
+
+### Auto-logistic model using 'autocov_dist' in 'spdep' package
+lm1 <- logistic.regression(df1, y='dis', x=c('hist'),
+                              autologistic=TRUE, coords=coordinates(df1), bw=5000) 
+
+### Auto-logistic model using 'autocov_dist' in 'spdep' package
+lm2 <- logistic.regression(df1, y='dis', x=c('weight'),
+                              autologistic=TRUE, coords=coordinates(df1), bw=5000) 
+
+### Auto-logistic model using 'autocov_dist' in 'spdep' package
+lm3 <- logistic.regression(df1, y='dis', x=c('smoke'),
+                              autologistic=TRUE, coords=coordinates(df1), bw=5000) 
+
+### Auto-logistic model using 'autocov_dist' in 'spdep' package
+lm4 <- logistic.regression(df1, y='dis', x=c('conc'),
+                              autologistic=TRUE, coords=coordinates(df1), bw=5000) 
 
 #==========================================================================================
 # Assess spatial auto-correlation
@@ -78,6 +102,7 @@ library(ape)
   m1 <- Moran.I(df1$weight, df1.dists.inv)
   m2 <- Moran.I(df1$hist, df1.dists.inv)
   m3 <- Moran.I(df1$smoke, df1.dists.inv)
+  m4 <- Moran.I(df1$dis, df1.dists.inv)
   
   #=================================================================================
   # Attempt 2 - adjusting for adjacent nearest neighbors
@@ -90,50 +115,29 @@ library(ape)
   neib.df1 <- knn2nb(knea.df1)
   
   # Get the mean of the adjacent residuals
-  resnb.df1 <- sapply(neib.df1, function(x) mean(lm1$residuals[x]))
+  resnb.df1 <- sapply(neib.df1, function(x) mean(lm1$Residuals$res[x]))
+  resnb.df2 <- sapply(neib.df1, function(x) mean(lm2$Residuals$res[x]))
+  resnb.df3 <- sapply(neib.df1, function(x) mean(lm3$Residuals$res[x]))
+  resnb.df4 <- sapply(neib.df1, function(x) mean(lm4$Residuals$res[x]))
   
   # Plot residuals and the mean of adjacent residuals and see there is no independence of the 
   # residuals
-  plot(lm1$residuals, resnb.df1, xlab='Residuals History and Concentration', ylab='Mean adjacent residuals',col='blue')
-  plot(lm2$residuals, resnb.df1, xlab='Residuals Weight and Concentration', ylab='Mean adjacent residuals',col='red')
-  plot(lm3$residuals, resnb.df1, xlab='Residuals Smoking and Concentration', ylab='Mean adjacent residuals',col='green')
-  
+  plot(lm1$Residuals$res, resnb.df1, xlab='Residuals History and Concentration', ylab='Mean adjacent residuals',col='blue')
+  plot(lm2$Residuals$res, resnb.df2, xlab='Residuals Weight and Concentration', ylab='Mean adjacent residuals',col='red')
+  plot(lm3$Residuals$res, resnb.df3, xlab='Residuals Smoking and Concentration', ylab='Mean adjacent residuals',col='green')
+  plot(lm4$Residuals$res, resnb.df4, xlab='Residuals Disease and Concentration', ylab='Mean adjacent residuals',col='purple')
   
   # Add sampling weights and compute Monte Carlo Moran's Statistic
   # MC just calculates a sample distribution
   lw.1 <- nb2listw(neib.df1)
-  moran.mc(lm1$residuals, lw, 999)  
+  moran.mc(lm1$Residuals$res, lw.1, 999)  
   
   lw.2 <- nb2listw(neib.df1)
-  moran.mc(lm2$residuals, lw, 999) 
+  moran.mc(lm2$Residuals$res, lw.2, 999) 
   
   lw.3 <- nb2listw(neib.df1)
-  moran.mc(lm3$residuals, lw, 999) 
+  moran.mc(lm3$Residuals$res, lw.3, 999) 
   
-  #Spatial lag model hist
-  m1s = lagsarlm(df1$conc ~ df1$hist, data=df1, lw,tol.solve=1.0e-30)
-  df1$residuals.hist <- residuals(m1s)
-  moran.mc(df1$residuals.hist, lw, 999) # p value of 0.844 sucessfully removes spatial autocorrelation
-
-  #Spatial lag model weight
-  m2s = lagsarlm(df1$conc ~ df1$weight, data=df1, lw,tol.solve=1.0e-30)
-  df1$residuals.weight <- residuals(m2s)
-  moran.mc(df1$residuals.weight, lw, 999) # p value of 0.833 sucessfully removes spatial autocorrelation
-  
-  #Spatial lag model smoke
-  m3s = lagsarlm(df1$conc ~ df1$smoke, data=df1, lw,tol.solve=1.0e-30)
-  df1$residuals.smoke <- residuals(m3s)
-  moran.mc(df1$residuals.smoke, lw, 999) # p value of  0.898 sucessfully removes spatial autocorrelation
-  
-  
-  coordinates(df1) <- ~ lon + lat
-  
-  # Use K nearest neighbor to create a nearest neighbor list
-  resnb.df1 <- sapply(neib.df1, function(x) mean(df1$residuals.hist[x]))
-  resnb.df2 <- sapply(neib.df1, function(x) mean(df1$residuals.weight[x]))
-  resnb.df3 <- sapply(neib.df1, function(x) mean(df1$residuals,smoke[x]))
-  
-  plot(df1$residuals.hist, resnb.df1, xlab='Residuals History and Concentration', ylab='Mean adjacent residuals',col='blue')
-  plot(df1$residuals.weight, resnb.df1, xlab='Residuals Weight and Concentration', ylab='Mean adjacent residuals',col='red')
-  plot(df1$residuals.smoke, resnb.df1, xlab='Residuals Smoking and Concentration', ylab='Mean adjacent residuals',col='green')
+  lw.4 <- nb2listw(neib.df1)
+  moran.mc(lm4$Residuals$res, lw.4, 999) 
   
